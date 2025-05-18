7ac472bbc9bc349c1c9a1cddc6a506dea316c4aa
Aleksei Ivanovskii <60579820+Lkgramm@users.noreply.github.com>
2025-05-19 01:46:14 +0300

    Delete generate_hash.py

--- generate_hash.py
+++
@@ -1,9 +0,0 @@
-# generate_hash.py
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-plain_password = "password"  # или любой другой пароль
-hashed_password = pwd_context.hash(plain_password)
-
-print("Hashed password:", hashed_password)
